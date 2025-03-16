a6c43db139abb75baad3f0ed190363aa0f271bf4
Saptarshi Manna <mannasaptarshi641408@gmail.com>
2025-03-16 14:17:14 +0530

    Create musiclibary.py

---
+++ musiclibary.py
@@ -0,0 +1,8 @@
+music={"moral":"https://youtu.be/SHHWC7ZoL9I?si=XgDrJ2EkNDqo-o9L",
+       "levitating":"https://youtu.be/TUVcZfQe-Kw?si=k75Vw022pPZaruwv",
+       "believer":"https://youtu.be/W0DM5lcj6mw?si=7U6qSQGSsjpVuvk0",
+       "piya":"https://youtu.be/RFjJ-NUS0Ro?si=ee4Czvr4VSeFd5Xi",
+       "cheques":"https://youtu.be/4tywp83zkmk?si=EO67mxR69QcMY5Ys",
+       "skyfall":"https://youtu.be/DeumyOzKqgI?si=4v1g-G_fQAwTpQah",
+       "coldplay":"https://youtu.be/YykjpeuMNEk?si=-vkLGhqlcvsE0-FT",
+       "deewane":"https://youtu.be/B-oZ64A_TeQ?si=-1-vznLbUd7VM4fQ"}
