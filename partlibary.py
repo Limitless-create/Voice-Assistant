73fe762cf91d0bc2ff1abfed40f5c49f38fff93c
Saptarshi Manna <mannasaptarshi641408@gmail.com>
2025-03-16 14:18:00 +0530

    Create partlibary.py

---
+++ partlibary.py
@@ -0,0 +1,4 @@
+clip={"moral":"https://youtube.com/shorts/N3FW35ZOnxc?si=3bxaU-hU3n3GbTlw",
+      "piya":"https://youtube.com/shorts/yHUmwsOkbG0?si=5deuQJvDSuSxESWJ",
+      "deewane":"https://youtube.com/shorts/5hhNpxk8H44?si=66D32N77E1QrFtUu",
+      "tu kahan":"https://youtube.com/shorts/n0NEOVThPE0?si=9Kmn5Vj9k7QIZiBN"}
